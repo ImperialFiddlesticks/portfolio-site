6f7736e2a45aa0bd1f062aff487c172a01a62030
ImperialFiddlesticks <bleckert.andrea@gmail.com>
2026-03-19 21:17:06 +0100

    changed source url for emails

--- api/contact.ts
+++ api/contact.ts
@@ -15,7 +15,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   try {
     await resend.emails.send({
-      from: "Portfolio Contact <onboarding@resend.dev>",
+      from: "Portfolio Contact <https://portfolio-site-lyart-alpha.vercel.app/>",
       to: "bleckertandrea@gmail.com",
       subject: `New message from ${name}`,
       text: `Name: ${name}\nEmail: ${email} \n\nMessage: \n${message}`,
